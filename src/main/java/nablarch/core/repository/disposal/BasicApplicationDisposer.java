f3f72b856ab0f4b28060778793bce3f393e680a2
opengl-8080 <opengl-8080@users.noreply.github.com>
2020-09-03 18:44:08 +0900

    fix: マルチスレッド下で安全に操作できるように、メソッドをsynchronizedに変更

--- src/main/java/nablarch/core/repository/disposal/BasicApplicationDisposer.java
+++ src/main/java/nablarch/core/repository/disposal/BasicApplicationDisposer.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 /**
  * {@link Disposable}を実装したコンポーネントを指定した順序で廃棄するクラス。<br>
- *
+ * <p>
+ * このクラスはマルチスレッド下で安全に操作できるように、すべてのメソッドを {@code synchronized} で宣言している。
+ * </p>
  * @author Tanaka Tomoyuki
  */
 public class BasicApplicationDisposer implements ApplicationDisposer {
@@ -17,7 +19,7 @@ public class BasicApplicationDisposer implements ApplicationDisposer {
     private List<Disposable> disposableList = new ArrayList<Disposable>();
 
     @Override
-    public void dispose() {
+    public synchronized void dispose() {
         /*
          * ■逆順に dispose している理由
          * 一般的に廃棄しなければならないオブジェクトは、開いた順番にインスタンスが生成される。
@@ -41,12 +43,12 @@ public class BasicApplicationDisposer implements ApplicationDisposer {
      * {@link Disposable}のリストを設定する。
      * @param disposableList {@link Disposable}のリスト
      */
-    public void setDisposableList(List<Disposable> disposableList) {
+    public synchronized void setDisposableList(List<Disposable> disposableList) {
         this.disposableList = new ArrayList<Disposable>(disposableList);
     }
 
     @Override
-    public void addDisposable(Disposable disposable) {
+    public synchronized void addDisposable(Disposable disposable) {
         disposableList.add(disposable);
     }
 }
